23e8c3e060cff904e51b16e42d13b1b29683068b
dirkster98 <ethan.dirks@gmail.com>
2020-01-21 15:37:41 -0800

    attempted ch 7 ex 3

--- chapter-07-exercises/exercise-3/exercise.R
+++ chapter-07-exercises/exercise-3/exercise.R
@@ -2,20 +2,21 @@
 
 # Create a vector `words` of 6 (or more) words.
 # You can Google for a "random word generator" if you wish!
-
+words <- c("cat","dog","possum","mouse","bunny","turtle")
 
 # Create a vector `words_of_the_day` that is your `words` vector with the string
 # "is the word of the day!" pasted on to the end.
 # BONUS: Surround the word in quotes (e.g., `'data' is the word of the day!`)
 # Note that the results are more obviously correct with single quotes.
-
+words_of_the_day <- c(words,  "is the word of the day")
+print(words_of_the_day)
 
 # Create a vector `a_f_words` which are the elements in `words` that start with 
 # "a" through "f"
 # Hint: use a comparison operator to see if the word comes before "f" alphabetically!
 # Tip: make sure all the words are lower-case, and only consider the first letter
 # of the word!
-
+a_f_words <- words()
 
 # Create a vector `g_m_words` which are the elements in `words` that start with 
 # "g" through "m"
